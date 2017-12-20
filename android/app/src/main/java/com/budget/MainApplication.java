feaad3232d84445bae3758317658347225cf5d85
Tony Holdstock-Brown <tony@cvstom.co>
2017-12-20 12:29:13 +0000

    Fix android build

--- android/app/src/main/java/com/budget/MainApplication.java
+++ android/app/src/main/java/com/budget/MainApplication.java
@@ -3,11 +3,11 @@ package com.budget;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.github.wuxudong.rncharts.MPAndroidChartPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.github.wuxudong.rncharts.MPAndroidChartPackage;
 
 import java.util.Arrays;
 import java.util.List;
